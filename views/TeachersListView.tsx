f0a9928c65a4ebb8fb7bc135fe32e791eaf999ea
mohammad maso <mohammadmasoudie@gmail.com>
2021-08-17 19:17:06 +0430

    o lala

--- views/TeachersListView.tsx
+++ views/TeachersListView.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Spinner } from '@chakra-ui/react';
 import TeacherList from '../componenets/TeacherList';
 import { useAllTeacherQuery } from '../graphql/generated/types';
+import ApiLoading from '../componenets/ApiLoading';
+import ApiError from '../componenets/ApiError';
 
 const TeachersListView = () => {
   const { loading, error, data } = useAllTeacherQuery();
-  if (loading) return <Spinner mt={3} color="primary" />;
-  if (error) return <p>خطا</p>;
+
+  if (loading) return <ApiLoading />;
+  if (error) return <ApiError />;
   return <>{data && <TeacherList {...data} />}</>;
 };
 
